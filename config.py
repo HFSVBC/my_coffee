2edd9a23b2f0c666d26e15d8a88256811b5f3f9c
Hugo Curado <hfsvbc@hugocurado.info>
2019-05-10 00:58:30 +0100

    Add secret key

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = '\x91\xea\x89\x0b\xe3N\x18\xf4\xb1\xfe{5\x97\xc5K\xcep\xa5n\x1a6\xae\x7f\x86'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
